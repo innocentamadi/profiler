7d16c4d2623bc8dfd7c79d8ca5bd01604cf3bd57
Innocent <innocent.amadi@andela.com>
2015-11-08 00:33:54 +0100

    adds float button

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,35 @@
 class UsersController < ApplicationController
+
+
+  def edit_user
+    @option = params[:option]
+    respond_to do |format|
+      format.js { render "show_edit_box.js" }
+    end if @option
+  end
+
+  def update_user
+    @option = (user_params && user_params[:option]) ? user_params[:option] : nil
+    update_user_info if @option
+    respond_to do |format|
+      format.js { render "update_user.js" }
+    end
+  end
+
+
+  def update_user_info
+    if @option == "fullname"
+      user_fullname
+    else
+      @current_user[@option] = user_params[@option]
+    end
+    @current_user.save
+  end
+
+  def user_fullname
+    fullname = user_params[@option].split(" ")
+    @current_user.first_name = fullname[0]
+    @current_user.last_name = fullname[1]
+  end
+
 end
