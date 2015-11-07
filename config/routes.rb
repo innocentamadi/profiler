a40f4a33e7fa6d036a580a3bdbf7ac13551d7073
Innocent <innocent.amadi@andela.com>
2015-11-07 22:11:12 +0100

    fixes profile page

--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,11 @@ Rails.application.routes.draw do
 
   resources :users
   resources :portfolio
+  root "welcome#index"
 
   resources :profile
 
   # The priority is based upon order of creation: first created -> highest priority.
-  root "welcome#index"
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
