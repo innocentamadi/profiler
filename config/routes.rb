94d0a7a8f40a5edf55ba3a5973be14d45959ba64
Innocent <innocent.amadi@andela.com>
2015-11-07 16:03:40 +0100

    adds portfolio page

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root "welcome#index"
   get "welcome/login" => "welcome#login", as: :login
 
+  resources :portfolio
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
