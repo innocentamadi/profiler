b033c8ed8096880f985074e7ee93befe278ee805
Kayode Adeniyi <kayode.adeniyi@andela.co>
2015-11-07 16:25:57 +0100

    Add users route

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  root "welcome#index"
   get "welcome/login" => "welcome#login", as: :login
-
+  resources :users
   # The priority is based upon order of creation: first created -> highest priority.
+  root "welcome#index"
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
