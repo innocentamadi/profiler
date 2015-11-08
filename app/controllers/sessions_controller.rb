8052cd704c5cca6e3403e5c3c3e03a9a1e726e55
Kayode Adeniyi <kayode.adeniyi@andela.co>
2015-11-08 14:08:43 +0100

    Added integration to linkedin

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,7 @@ class SessionsController < ApplicationController
   def create
     @omniauth_response = env['omniauth.auth']
     user = User.from_omniauth(@omniauth_response)
-    User.fetch_repo(@omniauth_response.info.nickname, user.id)
+    User.fetch_repo(@omniauth_response.info.nickname, user.id) if @omniauth_response.provider == 'github'
 
     session[:user_id] = user.id
     if env['omniauth.origin'].nil?
