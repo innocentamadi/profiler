16fd6e57e30762b10516b048650db585f9031475
Seyi Talabi <talabi.oluwaseyi@andela.com>
2015-11-09 11:49:14 +0100

    remove bootstrap-editable

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,12 +3,6 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-<<<<<<< HEAD
-=======
-  helper_method :xeditable?
-
-
->>>>>>> 4e7f87c193550794535628a2c1582a79ca2410cd
   helper_method :current_user
     protect_from_forgery with: :exception
 
